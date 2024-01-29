b40dfefae6f6f6c264675571fc05134a7c026401
Arthur C <arthurdourado01@hotmail.com>
2024-01-29 18:45:44 -0300

    componente de rotas

---
+++ src/billingCycle/billingCycle.jsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+export default props => (
+    <div>
+        <h1>Cliclo de Pagamentos</h1>
+    </div>
+)
---
+++ src/common/template/footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default props => (
+    <footer className="main-footer">
+        <strong>
+            Copyright &copy; 2024
+            <a href="http://coder.com.br" target="_blank"> Cod3r</a>.
+        </strong>
+    </footer>
+)
